abc93f1caa345490b2152a638d9f66255d8ee3b0
wolnio <kowolniak@gmail.com>
2021-11-25 10:24:19 +0100

    Update views.py

--- myapi/views.py
+++ myapi/views.py
@@ -4,7 +4,6 @@ from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from rest_framework.decorators import api_view
-# from rest_framework import viewsets     # maybe delete it?
 
 from .serializers import CarSerializer, CarRateSerializer
 from .models import Car, CarRate
